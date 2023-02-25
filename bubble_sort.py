e3ea6824817f117ca13aba5ad317cd67262f3a57
ilyes-guy <ilyes.guy@gmail.com>
2023-02-25 22:53:11 +0100

    bubble array complete

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,29 +1,33 @@
 import random
 
 
-array_lenght = 20
-arr = [0] * array_lenght
-#arr = [5, 7, 6, 2, 3, 7, 4, 4, 9, 63]
 
-
-def generate_array():
+def generate_array(array_lenght):
+    arr = [0] * array_lenght
     for i in range(0, array_lenght):
         arr[i]= random.randrange(1, 100)
-generate_array()
+    return arr
 
 
 
-
-
-run = True
-while(run):
-    co = 0
-    for i in range(array_lenght-1):
-        if(arr[i] > arr[i+1]):
-            arr[i], arr[i+1] = arr[i+1], arr[i]
-            co += 1
+def bubble_sort(arr):
+    run = True
+    while(run):
+        co = 0
+        for i in range(len(arr)-1):
+            if(arr[i] > arr[i+1]):
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                co += 1
         if(co == 0):
             run = False
+    return arr
 
 
-print(arr)
+
+
+
+
+if __name__ == "__main__":
+    my_array = generate_array(20)
+    print('unsorted array : ', my_array)
+    print('sorted array : ', bubble_sort(my_array))
