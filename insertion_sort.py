0efe4b788a947c7f8049013f007b6694e33cc5aa
ilyes-guy <ilyes.guy@gmail.com>
2023-02-26 20:43:06 +0100

    selection insertion

--- insertion_sort.py
+++ insertion_sort.py
@@ -11,16 +11,15 @@ def generate_array(array_lenght, number_lenght):
 
 
 
-def selection_sort(arr):
-    for i in range(len(arr)):
-        min_idx = i
-        for j in range(i+1, len(arr)):
-            if arr[min_idx] > arr[j]:
-                min_idx = j
-                  
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i-1
+        while j >= 0 and key < arr[j] :
+                arr[j + 1] = arr[j]
+                j -= 1
+        arr[j + 1] = key
     return arr
-
 
 
 
@@ -30,5 +29,5 @@ if __name__ == "__main__":
     my_array = generate_array(10, 1000) 
     print('unsorted array : ', my_array)
     start_time = time.time()
-    print('sorted array : ', selection_sort(my_array))
+    print('sorted array : ', insertion_sort(my_array))
     print("--- execution time --- %s seconds ---" % (time.time() - start_time))
