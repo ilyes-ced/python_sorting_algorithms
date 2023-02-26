828380720909dccdb66c4f826fba046d198a8486
ilyes-guy <ilyes.guy@gmail.com>
2023-02-26 19:05:49 +0100

    heap

--- heap_sort.py
+++ heap_sort.py
@@ -6,23 +6,40 @@ import time
 def generate_array(array_lenght, number_lenght):
     arr = [0] * array_lenght
     for i in range(0, array_lenght):
-        arr[i]= random.randrange(1, number_lenght)
+        arr[i] = random.randrange(1, number_lenght)
     return arr
 
 
 
+def heapify(arr, N, i):
+    largest = i
+    l = 2 * i + 1  
+    r = 2 * i + 2
+  
+    if l < N and arr[largest] < arr[l]:
+        largest = l
+  
+    if r < N and arr[largest] < arr[r]:
+        largest = r
+  
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+  
+        heapify(arr, N, largest)
+  
+  
+  
 def heap_sort(arr):
-    run = True
-    while(run):
-        co = 0
-        for i in range(len(arr)-1):
-            if(arr[i] > arr[i+1]):
-                mid = (arr[i] + arr[i+1]) / 2
-                co += 1
-        if(co == 0):
-            run = False
+    N = len(arr)
+  
+    for i in range(N // 2 - 1, -1, -1):
+        heapify(arr, N, i)
+  
+    for i in range(N-1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
     return arr
-
+  
 
 
 
